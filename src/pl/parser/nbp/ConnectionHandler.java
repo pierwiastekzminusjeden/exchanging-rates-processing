5cde85545267abd265eea2fe8405a815f9f2a445
Krystian Molenda <krystian.molenda@gmail.com>
2019-04-29 00:38:06 +0200

    xml list with selected urls added

--- src/pl/parser/nbp/ConnectionHandler.java
+++ src/pl/parser/nbp/ConnectionHandler.java
@@ -7,22 +7,22 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 
 public class ConnectionHandler {
     RunArgsHandler args;
     ArrayList<URL> dirsURLs;
-    ArrayList<String> xmlList;
+    ArrayList<URL> xmlURLs;
 
     ConnectionHandler(RunArgsHandler args) {
         this.args = args;
         dirsURLs = new ArrayList<>();
-        xmlList = new ArrayList<>();
-        buildDirsList();
+        xmlURLs = new ArrayList<>();
+        prepareDirs();
     }
 
-    private void buildDirsList() {
-
+    private void prepareDirs() {
         int begYear = args.getBegDate().getYear();
         int endYear = args.getEndDate().getYear();
         String tmp;
@@ -47,23 +47,50 @@ public class ConnectionHandler {
             }
         } catch (MalformedURLException e) { System.err.print(e.getStackTrace()); }
         //print to check
-        dirsURLs.stream().forEach(System.out::println);
+//        dirsURLs.stream().forEach(System.out::println);
     }
 
-    public void run() {
+        public void searchingDirs() {
         BufferedReader in;
         String inputLine;
+        String regex = "c[0-9]{3}z[0-9]{6}";
+        String tmp = "" + args.getBegDate().getYear()
+                + String.format("%02d", args.getBegDate().getMonthValue())
+                + String.format("%02d", args.getBegDate().getDayOfMonth());
+        int begDate = Integer.parseInt(tmp.substring(2));
+
+        tmp = "" + args.getEndDate().getYear()
+                + String.format("%02d", args.getEndDate().getMonthValue())
+                + String.format("%02d", args.getEndDate().getDayOfMonth());
+        int endDate= Integer.parseInt(tmp.substring(2));
+
 
         try {
             for (URL i : dirsURLs) {
                 in = new BufferedReader(new InputStreamReader(i.openStream()));
-
-                while ((inputLine = in.readLine()) != null)
-                    System.out.println(inputLine);
+                in.lines()
+                        .filter( line-> Pattern.matches(regex,line))
+                        .filter(line ->  Integer.parseInt(line.substring(5)) >= begDate && Integer.parseInt(line.substring(5)) <= endDate)
+                        .map(this::createXMLURL)
+                        .forEach(url -> xmlURLs.add(url));
                 in.close();
-
             }
+            System.out.println();
+            xmlURLs.stream().forEach(System.out::println);
+
         } catch (IOException e) { System.err.print(e.getStackTrace()); }
+    }
+
+    private URL createXMLURL(String data){
+        try{
+            return new URL("http://www.nbp.pl/kursy/xml/"+data+".xml");
+
+        }
+        catch(MalformedURLException e){
+            System.err.print(e.getStackTrace());
+        }
+        return null;
+
     }
 
 
--- src/pl/parser/nbp/MainClass.java
+++ src/pl/parser/nbp/MainClass.java
@@ -9,7 +9,7 @@ public class MainClass {
         if(args.length == 3){
             RunArgsHandler data = new RunArgsHandler(args[0], args[1], args[2]);
             ConnectionHandler ch = new ConnectionHandler(data);
-            ch.run();
+            ch.searchingDirs();
         }
         else{
             System.err.print("Wrong input");
